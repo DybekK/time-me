1ae56bbaa24b5c024bdd335842c3384a2abc72cb
mateuszdybowski <matdybowski@gmail.com>
2021-04-13 19:41:51 +0200

    initial commit

---
+++ src/main/java/com/dybek/timeme/TimeMeApplication.java
@@ -0,0 +1,13 @@
+package com.dybek.timeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dybek/timeme/TimeMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dybek.timeme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
