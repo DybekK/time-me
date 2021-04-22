42cae73a1e8ffe699cecbdc2abf4e958c098bf4f
mateuszdybowski <matdybowski@gmail.com>
2021-04-22 18:11:34 +0200

    moved UserSecurity content to KeycloakUserService

--- src/main/java/com/dybek/timeme/keycloak/KeycloakUserService.java
+++ src/main/java/com/dybek/timeme/keycloak/KeycloakUserService.java
@@ -1,9 +1,15 @@
 package com.dybek.timeme.keycloak;
 
+import com.dybek.timeme.exception.SecurityContextUserNotFoundException;
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.representations.AccessToken;
 import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import javax.ws.rs.core.Response;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -12,6 +18,20 @@ public class KeycloakUserService {
 
     public KeycloakUserService(KeycloakUserRepository keycloakUserRepository) {
         this.keycloakUserRepository = keycloakUserRepository;
+    }
+
+    public UserRepresentation getLoggedUser() throws SecurityContextUserNotFoundException {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        return Optional.ofNullable(securityContext.getAuthentication())
+                .flatMap(authentication -> {
+                    if (authentication.getPrincipal() instanceof KeycloakPrincipal) {
+                        KeycloakPrincipal<?> principal = (KeycloakPrincipal<?>) authentication.getPrincipal();
+                        Optional<AccessToken> accessToken = Optional.ofNullable(principal.getKeycloakSecurityContext().getToken());
+                        return accessToken.map(token -> keycloakUserRepository.find(UUID.fromString(token.getSubject())));
+                    }
+                    return Optional.empty();
+                })
+                .orElseThrow(() -> new SecurityContextUserNotFoundException("User was not found in security context"));
     }
 
     public UserRepresentation getUser(UUID id) {
